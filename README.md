ecfad3f138c83519c2529355669c104d7152c103
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-11-30 15:42:20 +0700

    UnitDay 6,7

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ ng new <tên-project>
 2.Chạy dự án
 ng serve 		 	        Mặc định dự án sẽ chạy ở port 4200, nếu bạn muốn đổi port thì chỉ cần thêm tham số
 ng serve --port=other-port 	Ví dụ: ng serve --port=9000
+3.tạo component trước
+ng g c progress-bar
+ng generate component progress-bar
--- ThienAn/src/app/app.module.ts
+++ ThienAn/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.componment';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
+    ProgressBarComponent,
   ],
   imports: [
     BrowserModule,
