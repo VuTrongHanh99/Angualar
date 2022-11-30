5df9b6705cc4d8ca9e7c486c3c2abb141e64e9f0
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-11-30 14:16:51 +0700

    UnitDay4,5: ngIf and ngFor

--- ThienAn/src/app/app.component.ts
+++ ThienAn/src/app/app.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ThienAn';
+  authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+    // more data
+  ];
   userObj = {
     name: 'Vu Hanh',
     age: 24,
