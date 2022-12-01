7719646b9bdc137e970b1e4c5d832db6fbf85ee8
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-12-02 01:14:22 +0700

    complete Bai14

--- Bai14NgTemplate_container/src/app/app.component.ts
+++ Bai14NgTemplate_container/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Bai14NgTemplate_container';
   counter=17;
+  navs=['Active','Link 1', 'Link2', 'LINK99'];
 }
---
+++ Bai14NgTemplate_container/src/app/tab-container/tab-container.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'tab-container',
+  template: `
+  <nav class="navbar navbar-expand-lg navbar-light bg-light mx-4 my-2">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item" *ngFor="let item of navs">
+        <a class="nav-link" href="#">
+        <ng-container *ngIf="headerTemplate;else noTemplate">
+        <ng-container *ngTemplateOutlet="headerTemplate || defaultTabButtons;" 
+        >
+        </ng-container>
+        </ng-container>
+        <ng-template #noTemplate>{{item}}</ng-template>
+        </a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+  </nav>
+  `,
+})
+export class TabContainerComponent {
+  @Input() navs!: string[]; 
+  // Custom template provided by parent
+  @Input() headerTemplate!: TemplateRef<any>; // Custom template provided by parent
+  defaultTabButtons=`<h1></h1>`
+  //context:{$implicit:link}
+
+}
