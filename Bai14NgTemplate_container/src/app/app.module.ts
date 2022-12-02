7fc27981a171882faa0c903cc1dcd7ca8fc8b6c4
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-12-02 10:07:35 +0700

    complete Bai16

--- Bai14NgTemplate_container/src/app/app.module.ts
+++ Bai14NgTemplate_container/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabContainerComponent } from './tab-container/tab-container.component';
+import { TabGroupComponent } from './tab-group/tab-group.component';
+import { BsTabGroupComponent } from './bs-tab-group/bs-tab-group.component';
+import { TabPanelComponent } from './tab-panel/tab-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabContainerComponent
+    TabContainerComponent,
+    TabGroupComponent,
+    BsTabGroupComponent,
+    TabPanelComponent
   ],
   imports: [
     BrowserModule,
