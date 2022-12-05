72cf9a9b14f8d07f31581033be3cc02fdd43f02c
VuTrongHanh99 <vuhanhcntt899@gmail.com>
2022-12-05 10:45:26 +0700

    complete Bai18

--- Bai18Pipess/src/app/app.module.ts
+++ Bai18Pipess/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTitlePipe } from './pipe-example/app-title.pipe';
+import { IsAdultPipe } from './pipe-example/is-adult.pipe';
 import { PipeExampleComponent } from './pipe-example/pipe-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipeExampleComponent
+    PipeExampleComponent,
+    AppTitlePipe, 
+    IsAdultPipe 
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- Bai18Pipess/src/app/pipe-example/pipe-example.component.ts
+++ Bai18Pipess/src/app/pipe-example/pipe-example.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-pipe-example',
   templateUrl: './pipe-example.component.html',
   styleUrls: ['./pipe-example.component.css']
 })
-export class PipeExampleComponent {
+export class PipeExampleComponent implements OnInit {
+  now = '2020-06-24T09:00:00.000Z';
+  userId = "4321245";
+  userIdChangeAfterFiveSeconds = "14324";
+  users: User[] = [
+    {
+      name: "Tiep Phan",
+      age: 30
+    },
+    {
+      name: "Trung Vo",
+      age: 28
+    },
+    {
+      name: "Chau Tran",
+      age: 29
+    },
+    {
+      name: "Tuan Anh",
+      age: 16
+    }
+  ];
 
+
+  newUser: User;
+
+  constructor() {
+    this.newUser = new User();
+  }
+
+  ngOnInit() {    
+  }
+
+  addUser() {
+    this.users.push(this.newUser);
+    this.newUser = new User();
+  }
+
+  addUserByUpdateReference() {
+    this.users = [...this.users, this.newUser];
+    this.newUser = new User();
+  }
 }
